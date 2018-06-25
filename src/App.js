a64597873d073f196156c68aad771570b0c38c99
Ashish Nema <ashish.nema@wdadc.com>
2018-06-25 12:10:47 +0530

    tried refactor

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
 
 	render() {
 		const filteredRobots = this.state.robots.filter( robot => {
-			return robot.name.toLowerCase().includes(this.state.searchField)
+			return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
 		});
 		return (
 			<div className = 'tc'>
